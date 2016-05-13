31a18bf454d0716fd8f9a042216b25017c600bd0
Filip Bekic <Filip Bekic>
2016-05-13 20:44:29 +0200

    Fixed angular and view resolver

--- src/main/java/com/tseo/studiorum/web/controller/HomeController.java
+++ src/main/java/com/tseo/studiorum/web/controller/HomeController.java
@@ -1,14 +1,16 @@
 package com.tseo.studiorum.web.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class HomeController {
 	
 	@RequestMapping("/")
-    public String index() {
-        return "resources.index.html";
+	public String get() {
+        return "index";
     }
 
 }

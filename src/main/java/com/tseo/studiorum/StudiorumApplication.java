d6d2edca1d1f31b7870c3246a13f0688e6c70279
alxbxbx <acastankovic.bb@gmail.com>
2016-05-13 20:46:36 +0200

    M

--- src/main/java/com/tseo/studiorum/StudiorumApplication.java
+++ src/main/java/com/tseo/studiorum/StudiorumApplication.java
@@ -2,11 +2,18 @@ package com.tseo.studiorum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 @SpringBootApplication
 public class StudiorumApplication {
 	
 	public static void main( String[] args){
+		
+		final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+				"classpath:/META-INF/resources/", "classpath:/webapp/resources/",
+				"classpath:/static/" };
+		
+		
 		SpringApplication.run(StudiorumApplication.class, args);
 	}
 }
--- src/main/java/com/tseo/studiorum/web/controller/HomeController.java
+++ src/main/java/com/tseo/studiorum/web/controller/HomeController.java
@@ -1,14 +1,14 @@
 package com.tseo.studiorum.web.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 	
 	@RequestMapping("/")
     public String index() {
-        return "resources.index.html";
+        return "resources/index.html";
     }
 
 }

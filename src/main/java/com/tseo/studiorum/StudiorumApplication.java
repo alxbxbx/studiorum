f55df28a376a8299b7c8028bc9571020253d2ebf
alxbxbx <acastankovic.bb@gmail.com>
2016-05-14 01:53:51 +0200

    Delete unsued stuff

--- src/main/java/com/tseo/studiorum/StudiorumApplication.java
+++ src/main/java/com/tseo/studiorum/StudiorumApplication.java
@@ -2,18 +2,11 @@ package com.tseo.studiorum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 @SpringBootApplication
 public class StudiorumApplication {
 	
 	public static void main( String[] args){
-		
-		final String[] CLASSPATH_RESOURCE_LOCATIONS = {
-				"classpath:/META-INF/resources/", "classpath:/webapp/resources/",
-				"classpath:/static/" };
-		
-		
 		SpringApplication.run(StudiorumApplication.class, args);
 	}
 }

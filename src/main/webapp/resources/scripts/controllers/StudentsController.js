35a83772a418436e0da8d08532456025fe980eef
Filip Bekic <filip1994sm@gmail.com>
2016-05-16 16:56:43 +0200

    Added angular rest

--- src/main/webapp/resources/scripts/controllers/StudentsController.js
+++ src/main/webapp/resources/scripts/controllers/StudentsController.js
@@ -2,33 +2,47 @@
 
 angular.module('studiorum').controller('StudentsController', ['$scope', 'Restangular', '$uibModal', '$log', '_', function($scope, Restangular, $uibModal, $log, _) {
 
+	// Initialization
+	
 	$scope.user = {};
 	
 	$scope.user.isStudent = true;
 	
-	$scope.clickShowPopupUser = function () {
-		$("#addUserModal").modal("show");
-	}
-	
-	$scope.clickHidePopupUser = function () {
-		$("#addUserModal").modal("hide");
-	}
-	
-	$scope.clickSaveUser = function () {
-		console.log($scope.user);
-	}
-	
-	$scope.clickSaveStudent = function() {
-		console.log($scope.user);
-		if (200) {
-			// hide with success message
-		} else {
-			// show error
-		}
-	}
+	$scope.baseStudents = Restangular.all('students');
 	
 	Restangular.all("students").getList().then(function(students) {
 		$scope.students = students;
     });
+	
+	// On Click Events
+	
+	$scope.clickHideUser = function () {
+		$("#addUserModal").modal("hide");
+	}
+	
+	$scope.clickCreateUser = function () {	
+		$scope.user = {};
+		$("#addUserModal").modal("show");
+	}
+	
+	$scope.clickEditUser = function (id) {
+		console.log(id);
+		$scope.user = Restangular.one('students', id).get();
+		console.log($scope.user);
+		$("#addUserModal").modal("show");
+	}
+	
+	$scope.clickDeleteUser = function () {
+		
+	}
+	
+	$scope.clickSaveUser = function () {
+		if ($scope.user.id) {
+			
+		} else {
+			$scope.baseStudents.post($scope.user);
+		}
+		
+	}
 
 }]);

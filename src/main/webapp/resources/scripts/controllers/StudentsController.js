3f0faa50693a33f54e6244320511acaed6831da0
Filip Bekic <filip1994sm@gmail.com>
2016-05-17 00:19:57 +0200

    Fixed angular

--- src/main/webapp/resources/scripts/controllers/StudentsController.js
+++ src/main/webapp/resources/scripts/controllers/StudentsController.js
@@ -3,46 +3,62 @@
 angular.module('studiorum').controller('StudentsController', ['$scope', 'Restangular', '$uibModal', '$log', '_', function($scope, Restangular, $uibModal, $log, _) {
 	
 	// Initialization
+	
+	$scope.user = {};
+	$scope.user.isStudent = true;
 	$scope.baseStudents = Restangular.all('students');
 	
-	Restangular.all("students").getList().then(function(students) {
-		$scope.students = students;
-    });
+	loadListOfStudents();
 	
 	// On Click Events
 	
 	$scope.clickHideUser = function () {
-		$("#addUserModal").modal("hide");
+		$("#userModal_").modal("hide");
 	}
 	
 	$scope.clickCreateUser = function () {	
 		$scope.user = {};
 		$scope.user.isStudent = true;
 		$scope.user.isStudentCreate = true;
-		$("#addUserModal").modal("show");
+		$("#userModal_").modal("show");
 	}
 	
 	$scope.clickEditUser = function (id) {
 		Restangular.one("students", id).get().then(function(user) {
-		    $scope.user = user;
+			$scope.user = user;
 		    $scope.user.isStudent = true;
 		    $scope.user.isStudentEdit = true;
 		    $scope.user.dateOfBirth = new Date($scope.user.dateOfBirth);
-		    $("#addUserModal").modal("show");
+		    $("#userModal_").modal("show");
 		});
 	}
 	
-	$scope.clickDeleteUser = function () {
-		
+	$scope.clickDeleteUser = function (id) {
+		if (confirm("Are you sure?")) {
+			Restangular.one("students", id).remove().then(function() {
+				loadListOfStudents();
+			});
+		}
 	}
 	
 	$scope.clickSaveUser = function () {
 		if ($scope.user.id) {
-			
+			$scope.baseStudents.customPUT($scope.user).then(function() {
+				loadListOfStudents();
+				$("#userModal_").modal("hide");
+			});
 		} else {
-			$scope.baseStudents.post($scope.user);
+			$scope.baseStudents.post($scope.user).then(function() {
+				loadListOfStudents();
+				$("#userModal_").modal("hide");
+			});
 		}
-		
+	}
+	
+	function loadListOfStudents() {
+		Restangular.all("students").getList().then(function(students) {
+			$scope.students = students;
+	    });
 	}
 
 }]);
